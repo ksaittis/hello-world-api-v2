7b3ec174431baa43ee5743749c84a720cbf2d0f5
Konstantinos Saittis <ksaittis@gmail.com>
2023-03-14 23:15:16 +0000

    Updated shared deployment resources

--- terraform/deployments/eu-west-1/shared/backend.tf
+++ terraform/deployments/eu-west-1/shared/backend.tf
@@ -1,7 +1,18 @@
 terraform {
   backend "s3" {
-    bucket         = "hello-world-api-state-bucket"
-    key            = "shared"
-    region         = "eu-west-1"
+    bucket = "hello-world-api-v2-state-bucket"
+    key    = "shared"
+    region = "eu-west-1"
   }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  profile = "dev"
 }
--- terraform/deployments/eu-west-1/shared/ecr.tf
+++ terraform/deployments/eu-west-1/shared/ecr.tf
@@ -1,10 +1,15 @@
 variable "repository_name" {
-  type = string
+  type    = string
   default = "hello-world-api"
 }
+
+data "aws_caller_identity" "current" {}
+
 
 module "ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
-  repository_name = var.repository_name
+  repository_name                   = var.repository_name
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  repository_force_delete           = false
 }
