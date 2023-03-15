9225dca9f64c46352e7fdc1f96c338669eb1237e
Konstantinos Saittis <ksaittis@gmail.com>
2023-03-15 22:12:42 +0000

    Resolved issues with ecr repo creation

--- terraform/deployments/eu-west-1/shared/ecr.tf
+++ terraform/deployments/eu-west-1/shared/ecr.tf
@@ -1,10 +1,4 @@
-variable "repository_name" {
-  type    = string
-  default = "hello-world-api"
-}
-
 data "aws_caller_identity" "current" {}
-
 
 module "ecr" {
   source = "terraform-aws-modules/ecr/aws"
@@ -12,4 +6,22 @@ module "ecr" {
   repository_name                   = var.repository_name
   repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
   repository_force_delete           = false
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 19
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
